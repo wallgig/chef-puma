b833b4f8602fe69825b92f28e4e3fc005aee9516
Greg Fitzgerald <greg@gregf.org>
2014-01-27 19:04:22 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,26 +1,17 @@
 name             'puma'
-maintainer       'Yousef Ourabi'
-maintainer_email 'yourabi@gmail.com'
+maintainer       'Greg Fitzgerald'
+maintainer_email 'greg@gregf.org'
 license          'Apache v2.0'
 description      'Installs and configures puma'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1'
 
 recipe           "puma", "Install puma"
 
-depends          'monit'
-depends          'logrotate'
+%w{ runit logrotate }.each do |dep|
+  depends dep
+end
 
 %w{ ubuntu debian }.each do |os|
   supports os
 end
-
-attribute "puma/version",
-  :display_name => "Puma Version",
-  :description => "Puma Version to install",
-  :default => "2.7.0"
-
-attribute "puma/bundler_version",
-  :display_name => "Bundler Version",
-  :description => "Bundler Version to install",
-  :default => "1.3.5"
