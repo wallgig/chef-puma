b5bc91cd1247d0e76341b9fc95dee0c9dca29bab
Greg Fitzgerald <greg@gregf.org>
2014-01-29 23:19:42 -0500

    Get some tests going

--- Berksfile
+++ Berksfile
@@ -2,5 +2,7 @@ site :opscode
 metadata
 cookbook 'apt'
 cookbook 'minitest-handler'
+cookbook 'build-essential'
+cookbook 'poise-ruby', git: 'https://github.com/poise/poise-ruby.git'
 cookbook 'puma-test', path: 'test/integration/cookbooks/puma-test'
 
---
+++ test/integration/cookbooks/puma-test/metadata.rb
@@ -0,0 +1,10 @@
+name 'puma-test'
+maintainer       'Greg Fitzgerald'
+maintainer_email 'greg@gregf.org'
+license          'Apache2'
+description      'Tests for the puma  LWRP'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'poise-ruby'
+depends 'puma'
